b7020fda7dbc35e22e5a808fc5044d8b05d93ca8
crxproject <coderunnerz@gmail.com>
2015-10-27 13:12:43 +0100

    first commit

---
+++ gads.js
@@ -0,0 +1,8 @@
+/*!
+ * Pro-AdBlock
+ * https://github.com/crxproject/pro-adblock/
+ *
+ * Adblockers routinely block javascript files that have *.ads* in the name and hence,
+ * the variable will not be set if the adblocker is active.
+ */
+adBlockDetected = false;
