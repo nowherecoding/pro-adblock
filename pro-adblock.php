dc6a5e125672998a8455e610393713a491da04db
Sergej Theiss <coderunnerz@gmail.com>
2018-01-31 11:48:48 +0100

    Adjusting translations domain

--- pro-adblock.php
+++ pro-adblock.php
@@ -6,8 +6,7 @@
   Author: Sergej Theiss
   Author URI: https://github.com/crxproject/
   Version: 2.0.0-beta
-  Text Domain: proadblock
-  Domain Path: /lang
+  Text Domain: pro-adblock
   License: http://www.gnu.org/licenses/gpl-2.0.html
   
   Pro-AdBlock is a WordPress plugin that shows a warning message to users that have no adblocker enabled.
@@ -38,7 +37,7 @@ define( 'WP_PADB_VERSION', '2.0.0-beta' );
 define( 'PADB_URL', plugin_dir_url( __FILE__ ) );
 
 // load the plugin's translated strings
-load_plugin_textdomain( 'proadblock', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+load_plugin_textdomain( 'pro-adblock', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
 
 /**
  * Custom css setup based on the users choice
@@ -82,7 +81,7 @@ function padb_overlay() {
 	<div id="padb-modal" class="padb-style-<?php echo $options[ 'modal_style' ]; ?>">
 		<div id="padb-modal-box">
 			<div id="padb-modal-content"><?php echo wpautop( $options[ 'modal_message' ] ); ?></div>
-			<div id="padb-modal-footer"><span id="padb-modal-close">&#10008; <?php echo __( 'Close modal to enter website', 'proadblock' ); ?></span></div>
+			<div id="padb-modal-footer"><span id="padb-modal-close">&#10008; <?php _e( 'Close modal to enter website', 'pro-adblock' ); ?></span></div>
 		</div>
 	</div>
 	<?php
@@ -117,35 +116,35 @@ function padb_settings_init() {
 	register_setting( 'pluginPage2', 'padb_settings' );
 
 	add_settings_section(
-			'padb_pluginPage_section_0', __( 'Message', 'proadblock' ), 'padb_settings_section_callback_1', 'pluginPage1'
+			'padb_pluginPage_section_0', __( 'Message', 'pro-adblock' ), 'padb_settings_section_callback_1', 'pluginPage1'
 	);
 
 	add_settings_field(
-			'modal_message', __( 'Text', 'proadblock' ), 'padb_message_render', 'pluginPage1', 'padb_pluginPage_section_0'
+			'modal_message', __( 'Text', 'pro-adblock' ), 'padb_message_render', 'pluginPage1', 'padb_pluginPage_section_0'
 	);
 
 	add_settings_section(
-			'padb_pluginPage_section_1', __( 'Appearance', 'proadblock' ), 'padb_settings_section_callback_2', 'pluginPage2'
+			'padb_pluginPage_section_1', __( 'Appearance', 'pro-adblock' ), 'padb_settings_section_callback_2', 'pluginPage2'
 	);
 
 	add_settings_field(
-			'modal_box_bg_color', __( 'Background color', 'proadblock' ), 'padb_box_bg_color_render', 'pluginPage2', 'padb_pluginPage_section_1'
+			'modal_box_bg_color', __( 'Background color', 'pro-adblock' ), 'padb_box_bg_color_render', 'pluginPage2', 'padb_pluginPage_section_1'
 	);
 
 	add_settings_field(
-			'modal_font_color', __( 'Font color', 'proadblock' ), 'padb_font_color_render', 'pluginPage2', 'padb_pluginPage_section_1'
+			'modal_font_color', __( 'Font color', 'pro-adblock' ), 'padb_font_color_render', 'pluginPage2', 'padb_pluginPage_section_1'
 	);
 
 	add_settings_field(
-			'modal_link_color', __( 'Link color', 'proadblock' ), 'padb_link_color_render', 'pluginPage2', 'padb_pluginPage_section_1'
+			'modal_link_color', __( 'Link color', 'pro-adblock' ), 'padb_link_color_render', 'pluginPage2', 'padb_pluginPage_section_1'
 	);
 
 	add_settings_field(
-			'modal_link_color_hover', __( 'Link hover color', 'proadblock' ), 'padb_link_color_hover_render', 'pluginPage2', 'padb_pluginPage_section_1'
+			'modal_link_color_hover', __( 'Link hover color', 'pro-adblock' ), 'padb_link_color_hover_render', 'pluginPage2', 'padb_pluginPage_section_1'
 	);
 
 	add_settings_field(
-			'modal_style', __( 'Modal style', 'proadblock' ), 'padb_select_modal_style_render', 'pluginPage2', 'padb_pluginPage_section_1'
+			'modal_style', __( 'Modal style', 'pro-adblock' ), 'padb_select_modal_style_render', 'pluginPage2', 'padb_pluginPage_section_1'
 	);
 }
 
@@ -187,23 +186,23 @@ function padb_link_color_hover_render() {
 function padb_select_modal_style_render() {
 	$options = padb_get_option( 'padb_settings' );
 	?>
-	<label><input type="radio" name="padb_settings[modal_style]" value="1"<?php checked( 1, $options[ 'modal_style' ], true ); ?> /> <span><?php echo __( 'Box w/ transparent background', 'pro-adblock' ); ?></span></label><br />
-	<label><input type="radio" name="padb_settings[modal_style]" value="2"<?php checked( 2, $options[ 'modal_style' ], true ); ?> /> <span><?php echo __( 'Fully locked screen', 'pro-adblock' ); ?></span></label>
+	<label><input type="radio" name="padb_settings[modal_style]" value="1"<?php checked( 1, $options[ 'modal_style' ], true ); ?> /> <span><?php _e( 'Box w/ transparent background', 'pro-adblock' ); ?></span></label><br />
+	<label><input type="radio" name="padb_settings[modal_style]" value="2"<?php checked( 2, $options[ 'modal_style' ], true ); ?> /> <span><?php _e( 'Fully locked screen', 'pro-adblock' ); ?></span></label>
 	<?php
 }
 
 function padb_settings_section_callback_1() {
-	echo __( 'Display a custom text to users that have no adblocker enabled.', 'proadblock' );
+	echo __( 'Display a custom text to users that have no adblocker enabled.', 'pro-adblock' );
 }
 
 function padb_settings_section_callback_2() {
-	echo __( 'Set custom colors for the modal box.', 'proadblock' );
+	echo __( 'Set custom colors for the modal box.', 'pro-adblock' );
 }
 
 function padb_options_page() {
 	?>
 	<div class="wrap">
-		<h1><?php echo __( 'Pro-AdBlock Settings', 'proadblock' ); ?></h1>
+		<h1><?php _e( 'Pro-AdBlock Settings', 'pro-adblock' ); ?></h1>
 
 		<form action='options.php' method='post'>
 
@@ -229,7 +228,7 @@ function padb_options_page() {
 function padb_get_option( $values ) {
 	// load default options if no entry in database
 	$defaults = array(
-		'modal_message'			 => __( "<h1>You are not using an Adblocker?!</h1>\n\nAdvertising displayed on webpages can be a security risk. Currently, the advertising mostly consists of embedded third party content. These contents are not under the website's owner editorial control and add a repeatedly criminally exploited attack vector to the website. An adblocker protects your surfing. This site explicitly supports the usage of advertisement blockers. Please consider to use one!\n\nYou can find a listing of adblockers here:\n<strong><a href=\"http://crxproject.github.io/pro-adblock/lists.html\" target=\"_blank\">Pro-AdBlock (Adblocker Promotion)</a></strong>\n\nThank you for your attention.", 'proadblock' ),
+		'modal_message'			 => __( "<h1>You are not using an Adblocker?!</h1>\n\nAdvertising displayed on webpages can be a security risk. Currently, the advertising mostly consists of embedded third party content. These contents are not under the website's owner editorial control and add a repeatedly criminally exploited attack vector to the website. An adblocker protects your surfing. This site explicitly supports the usage of advertisement blockers. Please consider to use one!\n\nYou can find a listing of adblockers here:\n<strong><a href=\"http://crxproject.github.io/pro-adblock/lists.html\" target=\"_blank\">Pro-AdBlock (Adblocker Promotion)</a></strong>\n\nThank you for your attention.", 'pro-adblock' ),
 		'modal_box_bg_color'	 => 'e89900',
 		'modal_font_color'		 => 'fff',
 		'modal_link_color'		 => 'fff',
